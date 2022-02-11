709d65906ed5a5f3e2c52bc090395d77b3e08db7
eeeuns <lys312510@naver.com>
2022-02-12 05:35:15 +0900

    3.0
    close #9  #8  #7 #6

--- README.md
+++ README.md
@@ -11,10 +11,18 @@ download [release](https://github.com/EeeUnS/onsen-downloader/releases) `onsen-d
   - ffmpeg.exe
   - onsen.exe
   - urlLink.txt
-  - youtube-dl.exe
   (`onsen.exe` inside using`ffmpeg.exe` and `youtube-dl.exe`)
 2. redio url add to `urlLink.txt`. see example in `urlLink.txt`
-3. onsen.exe so make mp3
+3. onsen.exe so make ts & mp3
+
+After using onsen.exe, urlLink.txt is created. Here, the file url used to download the file is stored, and you can directly re-download it by entering the command from cmd.
+
+If there is a problem with the occurrence of an error, etc., try running it in the cmd window (the window does not turn off and the log remains) and put the error as an issue.
+
+Follow commands sequentially.
+
+ffmpeg -headers \"referer: https://www.onsen.ag/\" -i   fileurl  -c copy  createFileName1.ts
+ffmpeg -i createFileName1.ts -write_xing 0 -id3v2_version 0  createFileName2.mp3
 
 ------------------------
 
@@ -25,14 +33,24 @@ onsen 최신회차 라디오를 다운로드합니다.
   - ffmpeg.exe
   - onsen.exe
   - urlLink.txt
-  - youtube-dl.exe
   `onsen.exe`가 내부적으로 `ffmpeg.exe`, `youtube-dl.exe`를 사용합니다.)
 
 2. 듣는 onsen url를 텍스트파일 안에 넣습니다. txt 파일안에 있는 url을 보면 대충 넣어야하는 url이 어떤건지, 어떻게 넣어야하는지 감이 잡힙니다.
-3. `onsen.exe` 실행하면 mp3파일이 생성됩니다. 
+3. `onsen.exe` 실행하면 ts파일과 mp3파일이 생성됩니다. 
+
+onsen.exe 사용후에는 urlLink.txt 이 생성 되어있습니다. 여기에는 파일을 다운로드 받는데 사용하는 파일 url 이 저장되어있으며 cmd에서 커맨드를 입력해 직접 다시 다운받을 수 있습니다.
+
+아래 커맨드를 순차적으로 실행
+
+
+ffmpeg -headers \"referer: https://www.onsen.ag/\" -i   파일url  -c copy  생성파일명1.ts
+ffmpeg -i 생성파일명1.ts -write_xing 0 -id3v2_version 0  생성파일명2.mp3
 
 혹시 에러 발생등으로 문제가 있을시에는 cmd 창에서 실행을 해보고(창이 안꺼지고 로그가 남아있음) 에러를 이슈로 넣어주세요.
 
 
-
 Thanks to ㅋㅋ(210.197)
+
+
+
+
--- onsen.py
+++ onsen.py
@@ -1,31 +1,52 @@
+# -*- coding: UTF-8 -*-
 import requests
 import re
 import codecs
 import os
+import sys
+import io
 
-command = 'youtube-dl.exe  --add-header Accept:"*/*" --add-header Accept-Encoding:"gzip, deflate, br" --add-header Accept-Language:"ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6" --add-header Connection:"keep-alive" --add-header Host:"onsen-ma3phlsvod.sslcs.cdngc.net" --add-header Origin:"https://www.onsen.ag" --add-header Referer:"https://www.onsen.ag/" --add-header "sec-ch-ua: Google Chrome;v=95, Chromium;v=95, ;Not A Brand;v=99" --add-header sec-ch-ua-mobile:"?0" --add-header sec-ch-ua-platform:""Windows"" --add-header Sec-Fetch-Dest:"empty" --add-header Sec-Fetch-Mode:"cors" --add-header Sec-Fetch-Site:"cross-site" --add-header User-Agent:"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"  --extract-audio  --audio-format mp3 '
+#ffmpeg -i " + e + " -c copy " +name + '.ts'
+#"ffmpeg -i " + name + '.ts' + " -write_xing 0 -id3v2_version 0 " + name + '.mp3' )
+command = "ffmpeg -headers \"referer: https://www.onsen.ag/\""
+w = open("fileWepUrl.txt", "a")
 
 def getMP3(url):
     try:
-        req =requests.get(url)
-        b = req.text
-        a = re.search("http((?!http).)*m3u8", b)
-        if not a :
-            print("error")
+        urlSplit = url.split("/")
+        programName = urlSplit[len(urlSplit) - 1].strip()
+        print(programName)
 
-        e = codecs.decode(a.group(), 'unicode-escape')
-        print(e)
-        name = re.search("((?!\/).)*(?:(\.mp))", e)
-        name = name.group().split('.')[0]
-        output_file = "-o \"" + name +  ".mp3\"  "
-        print(output_file)
-        print(name)
-        
-        print(command + output_file + e)
-        os.system(command + output_file + e)
-    except Exception as e:
-        print(e)
+        req =requests.get(url)
+        reqText = req.text
+        # print(req)
+        # print(b)
+
+        express = "http((?!http).)*("+ programName + ")((?!http).)*m3u8"
+        print(express)
+        fileWepPath = re.search(express , reqText)
+        print(fileWepPath)
+        if not fileWepPath :
+            print("error")
+        fileWepPath = codecs.decode(fileWepPath.group(), 'unicode-escape')
+        print(fileWepPath)
+
+        fileName = re.search("((?!\/).)*(?:(\.mp))", fileWepPath)
+        print(fileName)
+        fileName = fileName.group().split('.')[0]
+
+        commandInput =  "-i " + fileWepPath + " "
+        commandOutput = " -acodec mp3 " + fileName + ".mp3"
+        os.system(command +" -i " + fileWepPath + " -c copy "  +fileName + '.ts')
+        os.system("ffmpeg -i " + fileName + '.ts' + " -write_xing 0 -id3v2_version 0 "  + fileName + '.mp3' )
+        w.write(fileWepPath + "\n")
+    except Exception as fileWepPath:
+        print(fileWepPath)
 
 f = open("urlLink.txt", 'r')
-for line in f:
+lines = f.readlines()
+for line in lines:
+    print(line)
     getMP3(line)
+f.close()
+w.close()
